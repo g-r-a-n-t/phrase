9469daa6c69a18351586c00858474be8fafda6e9
Grant Wuerker <gwuerker@gmail.com>
2019-10-11 19:19:53 +0900

    ipfs test ip

--- react-app/src/config.js
+++ react-app/src/config.js
@@ -1,14 +1,25 @@
+import process from 'process'
+
 import REGISTRY from './deployedContracts/Registry'
+
+// dev, test
+const NODE_ENV = process.env.NODE_ENV || 'test'
 
 export default {
   ipfs: {
     local: {
       Bootstrap: [
-        '/ip4/127.0.0.1/tcp/4003/ws/ipfs/QmPvEgzKziX8HkvveB2w51DmuHFANM2HLmjXrbf22aLMnr'
+        {
+          dev:  '/ip4/127.0.0.1/tcp/4003/ws/ipfs/QmPvEgzKziX8HkvveB2w51DmuHFANM2HLmjXrbf22aLMnr',
+          test: '/ip4/34.73.155.236/tcp/4003/ws/ipfs/QmfXZ3QnwguQZA5z4Z2Nm5AaQSiGf56Y8hikiSjLs7BSXD'
+        }[NODE_ENV]
       ]
     },
     remote: {
-      host: '/ip4/127.0.0.1/tcp/5002'
+      host: {
+        dev:  '/ip4/127.0.0.1/tcp/5002',
+        test: '/ip4/34.73.155.236/tcp/4002'
+      }[NODE_ENV]
     }
   },
   registry: {
